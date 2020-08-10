ef44f38b89b8b22c724ead488e867ce865571399
LcsMattos <lcsmattos11@gmail.com>
2020-08-09 23:32:00 -0300

    Projeto NLW2 - HTML, CSS, JavaScript (node)

---
+++ src/server.js
@@ -0,0 +1,26 @@
+//servidor
+const express = require("express")
+const server = express() //Devolve um objeto 
+
+const {pageLanding, pageStudy, pageGiveClasses, saveClasses} = require('./pages')
+
+//Configurar nunjucks (template engine)
+const nunjucks = require("nunjucks")
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+//início e configuração do servidor
+server
+//receber os dados do req.body
+.use(express.urlencoded({extended: true}))
+//configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+//start do servidor
+.listen(5500)
